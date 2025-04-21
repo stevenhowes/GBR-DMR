b81fad91d36077a67923d740a599fcd3f684d21a
Steve Howes <38082088+stevenhowes@users.noreply.github.com>
2025-04-21 21:54:41 +0100

    Cleanup in script and latest contact updates

--- contacts.sh
+++ contacts.sh
@@ -2,3 +2,4 @@
 rm -f GD77_WW_default.csv
 wget https://raw.githubusercontent.com/ContactLists/NORMAL-ContactLists/master/RADIODDITY/GD77/GD77_WW_default.csv
 egrep 'Radio ID|,GBR' GD77_WW_default.csv > GBR-Contacts.csv
+rm -f GD77_WW_default.csv
